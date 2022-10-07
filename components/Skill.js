e83b299f0ab43d36540f8b9cedcc0abb02253967
Abner Elias Ribeiro <eliasabner38@gmail.com>
2022-10-07 02:19:27 -0300

    added tag strong on skills

--- components/Skill.js
+++ components/Skill.js
@@ -3,7 +3,7 @@ import styles from'../styles/Skill.module.css'
 function Skill({name}) {
     return (
     <>
-        <span className={styles.span}>{name}</span>
+        <span className={styles.span}><strong>{name}</strong></span>
     </>
     );
 }
