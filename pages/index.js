7ff16c0f128ff8b14a16d65b469fb1a9fc073de2
Abneribeiro <eliasabner38@gmail.com>
2023-05-19 20:40:30 +0000

    updated

--- pages/index.js
+++ pages/index.js
@@ -22,8 +22,9 @@ export default function Home() {
           <title>AbneRibeiro</title>
           <meta
             name="description"
-            content="Introduction and summary of Abner Ribeiro projects. Web developer"
+            content="Discover actionable insights and unlock the full potential of your data with Data Analysis Hub. We provide cutting-edge tools and expertise to analyze, visualize, and interpret complex data sets."
           />
+          <meta property="og:image" content="/ab.png" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
           <meta http-equiv="X-UA-Compatible" content="IE=edge"></meta>
           <link rel="icon" href="/favicon.ico" />
