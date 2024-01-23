878cc83aa4f56f82d59ef281b39e021d2421a9d1
seu nome <eliasabner38@gmail.com>
2024-01-23 19:57:10 +0000

    Add Lottie animation and update Main component styles

--- components/Main.js
+++ components/Main.js
@@ -1,8 +1,20 @@
-
+import Lottie from "lottie-react";
+import animation from "../public/assets/animation.json";
+import styles from "../styles/Main.module.css";
 
 function Main() {
   return (
-          <h4>Main Content</h4>
+    <div className={styles.container}>
+      <div className={`${styles.flexItem} ${styles.content}`}>
+        <h1> I'm Abner</h1>
+        <p className={styles.lead}>Backend Developer</p>
+        <p>Seasoned backend developer crafting high-performance solutions with precision and a commitment to technical excellence.</p>
+        <a className={styles.emailButton} href="mailto:example@example.com">Get in touch</a>
+      </div>
+      <div className={`${styles.flexItem} ${styles.animation}`}>
+        <Lottie className={styles.animationContainer} animationData={animation} />
+      </div>
+    </div>
   );
 }
 
